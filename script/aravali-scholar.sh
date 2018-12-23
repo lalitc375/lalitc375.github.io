3369a9f5d35fb39e68ba73ada0bfd2ddc02c7a78
Lalit Chauhan <lalitc375@gmail.com>
2018-12-23 11:48:02 +0530

    Update aravali-scholar.sh

--- script/aravali-scholar.sh
+++ script/aravali-scholar.sh
@@ -2,6 +2,6 @@
 file="/tmp/user-passwords.txt"
 while IFS=' ' read  f1 f2
 do
-adduser --disabled-login --quiet $f1;
+adduser --disabled-login --gecos "" --quiet $f1;
 echo $f1:$f2 | chpasswd;
 done <"$file"
