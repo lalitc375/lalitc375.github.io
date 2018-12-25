f2094df011c76c875af12b375a4de640b6f87796
Lalit Chauhan <lalitc375@gmail.com>
2018-12-25 09:54:54 +0530

    Update aravali-scholar.sh

--- script/aravali-scholar.sh
+++ script/aravali-scholar.sh
@@ -1,8 +1,8 @@
-file="/var/aravaliScript/user-passwords.txt"
+file="/tmp/user-passwords1.txt"
 while IFS=' ' read  f1 f2
 do
 adduser --disabled-login --gecos "" --quiet $f1;
 echo $f1:$f2 | chpasswd;
 done <"$file"
 
-touch lalit123.txt
+touch /tmp/lalit123.txt
